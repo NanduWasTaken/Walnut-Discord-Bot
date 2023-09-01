8cf7ae101290b9f32637def231533549684c61aa
NanduWasTaken <nanduwastaken@gmail.com>
2023-09-01 07:05:41 +0000

    Improved🔥

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -5,23 +5,23 @@ module.exports = {
   cooldown: 5,
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Replies with Pong!")  
+    .setDescription("Replies with Pong!")
 /*.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)*/,
   async execute(interaction) {
     //console.log(interaction.guild);
-  /*  const newUserInstance = new Guild({
-        name: interaction.guild.name,
-        id: interaction.guild.id,
-        ownerId: interaction.guild.ownerId
-      });
-      await newUserInstance.save();*/
-    
+    /*  const newUserInstance = new Guild({
+          name: interaction.guild.name,
+          id: interaction.guild.id,
+          ownerId: interaction.guild.ownerId
+        });
+        await newUserInstance.save();*/
+
     const sent = await interaction.deferReply({ fetchReply: true });
-      
+
 
     return interaction.editReply(`Websocket HeartBeat: ${interaction.client.ws.ping}ms\nRoundtrip Latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
 
-    
-   
+
+
   },
 };
