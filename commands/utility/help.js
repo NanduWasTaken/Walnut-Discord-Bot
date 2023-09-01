fc7f033e8d4adeb64a14dade0b5318eed2397f4a
NanduWasTaken <NanduWasTaken@users.noreply.github.com>
2023-09-01 13:23:53 +0000

    ✨Formatted By Prettier Code✨
    ✨Prettier Code Formatter Configured By NanduWasTaken✨

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -11,56 +11,47 @@ module.exports = {
     .setName("help")
     .setDescription("Replies with help stuff!"),
   async execute(interaction) {
-
     const emojis = {
-      fun: '😃',
-      moderation: '⚔️',
-      utility: '📙',
-    }
+      fun: "😃",
+      moderation: "⚔️",
+      utility: "📙",
+    };
 
-    
     const commandInfo = interaction.client.commands.map((cmd) => {
-    const folder = cmd.folder;
-    return {
+      const folder = cmd.folder;
+      return {
         label: cmd.folder,
         value: cmd.folder.toLowerCase(),
         discription: `Commands From ${cmd.folder} Category.`,
         emoji: emojis[cmd.folder.toLowerCase() || null],
-    };
-});
+      };
+    });
 
-console.log(commandInfo);
-   // console.log(interaction.client.commands)
-    
+    console.log(commandInfo);
+    // console.log(interaction.client.commands)
 
-    
     const select = new StringSelectMenuBuilder()
       .setCustomId("help")
       .setPlaceholder("Choose Any Command Category!")
       .addOptions(commandInfo);
 
     const row = new ActionRowBuilder().addComponents(select);
-    
-    
 
-
-    
     const response = await interaction.reply({
       content: "Choose your starter!",
       components: [row],
     });
 
-  const collector = response.createMessageComponentCollector({ componentType: ComponentType.StringSelect, time: 3_600_000 });
+    const collector = response.createMessageComponentCollector({
+      componentType: ComponentType.StringSelect,
+      time: 3_600_000,
+    });
 
-collector.on('collect', async i => {
-	const selection = i.values[0];
-	await i.reply(`${i.user} has selected ${selection}!`);
-});
-    
-      
-    
+    collector.on("collect", async (i) => {
+      const selection = i.values[0];
+      await i.reply(`${i.user} has selected ${selection}!`);
+    });
+
     return;
-
   },
 };
-        
