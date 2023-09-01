a931f7f9915d7c40d7a6146d1ea4a031cd311f50
Nandu <nanduwastaken@gmail.com>
2023-09-01 18:53:31 +0530

    Update help.js

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -3,6 +3,7 @@ const {
   StringSelectMenuBuilder,
   StringSelectMenuOptionBuilder,
   SlashCommandBuilder,
+  ComponentType,
 } = require("discord.js");
 
 module.exports = {
@@ -10,36 +11,56 @@ module.exports = {
     .setName("help")
     .setDescription("Replies with help stuff!"),
   async execute(interaction) {
+
+    const emojis = {
+      fun: '😃',
+      moderation: '⚔️',
+      utility: '📙',
+    }
+
+    
+    const commandInfo = interaction.client.commands.map((cmd) => {
+    const folder = cmd.folder;
+    return {
+        label: cmd.folder,
+        value: cmd.folder.toLowerCase(),
+        discription: `Commands From ${cmd.folder} Category.`,
+        emoji: emojis[cmd.folder.toLowerCase() || null],
+    };
+});
+
+console.log(commandInfo);
+   // console.log(interaction.client.commands)
+    
+
+    
     const select = new StringSelectMenuBuilder()
-      .setCustomId("starter")
-      .setPlaceholder("Make a selection!")
-      .addOptions(
-        new StringSelectMenuOptionBuilder()
-          .setLabel("Bulbasaur")
-          .setDescription("The dual-type Grass/Poison Seed Pokémon.")
-          .setValue("bulbasaur"),
-        new StringSelectMenuOptionBuilder()
-          .setLabel("Charmander")
-          .setDescription("The Fire-type Lizard Pokémon.")
-          .setValue("charmander"),
-        new StringSelectMenuOptionBuilder()
-          .setLabel("Squirtle")
-          .setDescription("The Water-type Tiny Turtle Pokémon.")
-          .setValue("squirtle"),
-      );
+      .setCustomId("help")
+      .setPlaceholder("Choose Any Command Category!")
+      .addOptions(commandInfo);
 
     const row = new ActionRowBuilder().addComponents(select);
+    
+    
 
+
+    
     const response = await interaction.reply({
       content: "Choose your starter!",
       components: [row],
-
-      /* const collector = response.createMessageComponentCollector({ componentType: ComponentType.StringSelect, time: 3600000 });
-
-      collector.on('collect', async i => {
-	      const selection = i.values[0];
-        	await i.reply(`${i.user} has selected ${selection}!`);
-      });*/
     });
+
+  const collector = response.createMessageComponentCollector({ componentType: ComponentType.StringSelect, time: 3_600_000 });
+
+collector.on('collect', async i => {
+	const selection = i.values[0];
+	await i.reply(`${i.user} has selected ${selection}!`);
+});
+    
+      
+    
+    return;
+
   },
 };
+        
