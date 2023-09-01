f268dfa5a2ebc97581fe36b9d4a84986c6ce2e90
NanduWasTaken <nanduwastaken@gmail.com>
2023-09-01 03:26:12 +0000

    Prevented FAILED TO REPOUND

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -39,18 +39,22 @@ module.exports = {
 
     channel.send({embeds: [embed]})
 
-    const command = interaction.client.commands.get(interaction.commandName);
+   const command = interaction.client.commands.get(interaction.commandName);
 
-    if (!command) {
-      console.error(`No command matching ${interaction.commandName} was found.`);
-      return;
-    }
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
 
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(`Error executing ${interaction.commandName}`);
-      console.error(error);
-    }
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
   },
 };
