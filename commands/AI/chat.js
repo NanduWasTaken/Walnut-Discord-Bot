d060588383afded22b001365180edb475f8b2b71
NanduWasTaken <NanduWasTaken@users.noreply.github.com>
2023-09-14 15:26:35 +0000

    ✨Formatted By Prettier Code✨
    ✨Prettier Code Formatter Configured By NanduWasTaken✨

--- commands/AI/chat.js
+++ commands/AI/chat.js
@@ -3,32 +3,31 @@ const {
   PermissionFlagsBits,
   EmbedBuilder,
 } = require("discord.js");
-const fetch = require('@replit/node-fetch')
+const fetch = require("@replit/node-fetch");
 
 module.exports = {
   cooldown: 5,
   data: new SlashCommandBuilder()
     .setName("chat")
     .setDescription("Chat With An AI!")
-    .addStringOption(option =>
+    .addStringOption((option) =>
       option
-        .setName('msg')
-        .setDescription('Enter your message.')
-        .setRequired(true) 
+        .setName("msg")
+        .setDescription("Enter your message.")
+        .setRequired(true),
     ),
   async execute(interaction) {
+    const msg = interaction.options.getString("msg");
 
-  const msg = interaction.options.getString('msg');
-    
     const sent = await interaction.deferReply({ fetchReply: true });
 
-    const apiUrl = 'https://api.popcat.xyz/chatbot';
+    const apiUrl = "https://api.popcat.xyz/chatbot";
     const params = {
       owner: `[NanduWasTaken](https://github.com/NanduWasTaken)`,
       botname: interaction.client.user.username,
-      msg: msg,     
+      msg: msg,
     };
-    
+
     const queryParams = new URLSearchParams(params);
     const requestUrl = `${apiUrl}?${queryParams}`;
 
@@ -36,11 +35,12 @@ module.exports = {
 
     if (response.status !== 200) {
       throw new Error(`API request failed with status ${response.status}`);
-      return interaction.editReply('We Are Having Trouble With The Api, Please Try Again.');
+      return interaction.editReply(
+        "We Are Having Trouble With The Api, Please Try Again.",
+      );
     }
 
     const data = await response.json();
-
 
     return interaction.editReply(data.response);
   },
--- commands/Fun/waifu.js
+++ commands/Fun/waifu.js
@@ -27,40 +27,40 @@ module.exports = {
         ),
     ),
   async execute(interaction) {
-
-  const tag = interaction.options.getString('tags');
+    const tag = interaction.options.getString("tags");
 
     const tag = interaction.options.getString("tags");
 
-  try {
-    const sent = await interaction.deferReply({ fetchReply: true });
+    try {
+      const sent = await interaction.deferReply({ fetchReply: true });
 
-    const apiUrl = "https://api.waifu.im/search";
-    const params = {
-      included_tags: tag,
-      height: ">=2000",
-    };
+      const apiUrl = "https://api.waifu.im/search";
+      const params = {
+        included_tags: tag,
+        height: ">=2000",
+      };
 
-    const queryParams = new URLSearchParams(params);
-    const requestUrl = `${apiUrl}?${queryParams}`;
+      const queryParams = new URLSearchParams(params);
+      const requestUrl = `${apiUrl}?${queryParams}`;
 
-    const response = await fetch(requestUrl);
+      const response = await fetch(requestUrl);
 
+      if (response.status !== 200) {
+        throw new Error(`API request failed with status ${response.status}`);
+      }
 
-    if (response.status !== 200) {
-      throw new Error(`API request failed with status ${response.status}`);
+      const data = await response.json();
+
+      if (data.images && data.images.length > 0) {
+        return interaction.editReply(data.images[0].url);
+      } else {
+        return interaction.editReply("No images found for the specified tag.");
+      }
+    } catch (error) {
+      console.error(error);
+      return interaction.editReply(
+        "An error occurred while fetching data from the API. Please try again later.",
+      );
     }
-
-    const data = await response.json();
-
-    if (data.images && data.images.length > 0) {
-      return interaction.editReply(data.images[0].url);
-    } else {
-      return interaction.editReply("No images found for the specified tag.");
-    }
-  } catch (error) {
-    console.error(error);
-    return interaction.editReply("An error occurred while fetching data from the API. Please try again later.");
-  }
-},
+  },
 };
