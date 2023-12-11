48dfc586f3c0e9b63e0b04d3d6dfe616d497806e
SoftwareX Plus <119097812+softwarexplus@users.noreply.github.com>
2023-12-11 14:21:24 +0600

    Create test.ts

---
+++ src/commands/fun/test.ts
@@ -0,0 +1,22 @@
+import { ChatInputCommandInteraction } from "discord.js"
+import { ChatInputCommandData } from "../../type"
+import { uptime } from "coolcake"
+
+export const data: ChatInputCommandData = {
+    name: "test",
+    description: "Check Bot Status"
+}
+
+export async function run(interaction: ChatInputCommandInteraction) {
+    await interaction.deferReply()
+    const reply = await interaction.fetchReply()
+    const ping = reply.createdTimestamp - interaction.createdTimestamp
+    interaction.followUp({
+        embeds: [
+            {
+                color: 0x5865f2,
+                description: `Client: ${ping}ms \nWebsocket: ${interaction.client.ws.ping}ms \nUptime: ${uptime()}`
+            }
+        ]
+    })
+}
