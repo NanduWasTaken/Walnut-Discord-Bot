feaae80870c99446b28facfaa545247f979c1243
SoftwareX Plus <119097812+softwarexplus@users.noreply.github.com>
2023-12-11 14:20:45 +0600

    Create guild.ts

---
+++ src/schema/guild.ts
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose"
+
+const guild = new Schema({
+    name: {
+        type: String
+    },
+    id: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    ownerId: {
+        type: String
+    }
+})
+
+export default model("Guild", guild)
