22bef503e1741c701b8a22d55a1dd39afd4c57ad
SoftwareX Plus <119097812+softwarexplus@users.noreply.github.com>
2023-12-11 14:20:36 +0600

    Create server.ts

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express"
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen("7200", () => {
+    console.log(`[✅] Server running at http://localhost:7200`)
+})
