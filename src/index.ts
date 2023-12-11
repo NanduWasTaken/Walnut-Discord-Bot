e4e070b81ba0f308fcbd9c1512e317c4c2d5cdf5
SoftwareX Plus <119097812+softwarexplus@users.noreply.github.com>
2023-12-11 14:20:26 +0600

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { Client, GatewayIntentBits } from "discord.js"
+import { EventsHandler as Events } from "./function/handler"
+import { connect } from "mongoose"
+import config from "./config"
+import("./server.js")
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+})
+
+Events(client)
+client.login(config.TOKEN)
+if (config.DB_URI) {
+    connect(config.DB_URI).then(() => console.log("Connected to MongoDB Database"))
+}
+
+process.on("unhandledRejection", (reason: Error, p: Promise<any>) => {
+    console.trace(reason, p)
+})
+
+process.on("uncaughtException", (err: Error, origin: string) => {
+    console.trace(err, origin)
+})
+
+process.on("uncaughtExceptionMonitor", (err: Error, origin: string) => {
+    console.trace(err, origin)
+})
