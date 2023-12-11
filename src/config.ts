6a29db4a6d4bc0273940e2e338fd89cacf58df7a
softwarexplus <softwarexplus@users.noreply.github.com>
2023-12-11 08:27:00 +0000

    Formatted By Prettier Code

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,15 @@
 export default {
-    TOKEN: process.env.token,
-    DB_URI: process.env.DB_URI,
-    port: process.env.PORT,
-    help_category_icon: {
-        fun: "😃",
-        moderation: "⚒️",
-        utility: "📙",
-        ai: "🤖",
-        bot: "💥"
-    },
-    guildId: [""],
-    ownerId: "852381000528035890",
-    supportServer: "https://discord.gg/nPCNpEPWTh"
-}
+  TOKEN: process.env.token,
+  DB_URI: process.env.DB_URI,
+  port: process.env.PORT,
+  help_category_icon: {
+    fun: "😃",
+    moderation: "⚒️",
+    utility: "📙",
+    ai: "🤖",
+    bot: "💥",
+  },
+  guildId: [""],
+  ownerId: "852381000528035890",
+  supportServer: "https://discord.gg/nPCNpEPWTh",
+};
