7b375e62df1eaa5dd8ec20d7aa76f6075c9933a2
SoftwareX Plus <119097812+softwarexplus@users.noreply.github.com>
2023-12-11 14:17:14 +0600

    Delete consoleLog.js

--- events/ready/consoleLog.js
+++
@@ -1,9 +0,0 @@
-const { Events, ActivityType } = require("discord.js");
-
-module.exports = {
-  name: Events.ClientReady,
-  once: true,
-  async execute(client) {
-    console.log(`[✅] Logged in as ${client.user.tag}`);
-  },
-};
