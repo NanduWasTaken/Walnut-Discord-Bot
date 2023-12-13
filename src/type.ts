27ebc13df5801a46615869fa6dd7277c1d77b4e2
SoftwareX Plus <119097812+softwarexplus@users.noreply.github.com>
2023-12-13 10:50:45 +0600

    Update type.ts

--- src/type.ts
+++ src/type.ts
@@ -1,27 +1,24 @@
-import {
-  RESTPostAPIApplicationCommandsJSONBody,
-  PermissionResolvable,
-} from "discord.js";
+import { RESTPostAPIApplicationCommandsJSONBody, PermissionResolvable, SlashCommandBuilder } from "discord.js"
 
 export {
-  RESTPostAPIChatInputApplicationCommandsJSONBody as ChatInputCommandData,
-  RESTPostAPIContextMenuApplicationCommandsJSONBody as ContextMenuCommandData,
-} from "discord.js";
+    RESTPostAPIChatInputApplicationCommandsJSONBody as ChatInputCommandData,
+    RESTPostAPIContextMenuApplicationCommandsJSONBody as ContextMenuCommandData
+} from "discord.js"
 
 export interface CommandObject {
-  data: RESTPostAPIApplicationCommandsJSONBody;
-  option?: CommandOption;
-  path: CommandPath;
-  run: Function;
+    data: RESTPostAPIApplicationCommandsJSONBody | SlashCommandBuilder
+    option?: CommandOption
+    path?: CommandPath
+    run: Function
 }
 
 export interface CommandPath {
-  name: string;
-  category: string;
+    name: string
+    category: string
 }
 
 export interface CommandOption {
-  CoolDown?: number;
-  TestServer?: boolean;
-  BotPermission?: Array<PermissionResolvable>;
+    CoolDown?: number
+    TestServer?: boolean
+    BotPermission?: Array<PermissionResolvable>
 }
