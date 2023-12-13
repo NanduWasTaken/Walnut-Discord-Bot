011dc8c651fc0815352956ac08428a3f45ebc228
SoftwareX Plus <119097812+softwarexplus@users.noreply.github.com>
2023-12-13 10:49:58 +0600

    Update ping.ts

--- src/commands/fun/ping.ts
+++ src/commands/fun/ping.ts
@@ -1,24 +1,22 @@
-import { ChatInputCommandInteraction } from "discord.js";
-import { ChatInputCommandData } from "../../type";
-import { uptime } from "coolcake";
+import { ChatInputCommandInteraction } from "discord.js"
+import { ChatInputCommandData } from "../../type"
+import { uptime } from "coolcake"
 
 export const data: ChatInputCommandData = {
-  name: "ping",
-  description: "Replies with Pong!",
-};
+    name: "ping",
+    description: "Replies with Pong!"
+}
 
 export async function run(interaction: ChatInputCommandInteraction) {
-  await interaction.deferReply();
-  const reply = await interaction.fetchReply();
-  const ping = reply.createdTimestamp - interaction.createdTimestamp;
-  interaction.followUp({
-    embeds: [
-      {
-        color: 0x5865f2,
-        description: `Client: ${ping}ms \nWebsocket: ${
-          interaction.client.ws.ping
-        }ms \nUptime: ${uptime()}`,
-      },
-    ],
-  });
+    await interaction.deferReply()
+    const reply = await interaction.fetchReply()
+    const ping = reply.createdTimestamp - interaction.createdTimestamp
+    interaction.followUp({
+        embeds: [
+            {
+                color: 0x5865f2,
+                description: `Client: ${ping}ms \nWebsocket: ${interaction.client.ws.ping}ms \nUptime: ${uptime()}`
+            }
+        ]
+    })
 }
