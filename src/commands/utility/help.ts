d34713eea70377a1bc82f85e053b4669331a3f65
SoftwareX Plus <119097812+softwarexplus@users.noreply.github.com>
2023-12-13 10:50:04 +0600

    Update help.ts

--- src/commands/utility/help.ts
+++ src/commands/utility/help.ts
@@ -1,116 +1,110 @@
 import {
-  ActionRowBuilder,
-  ChatInputCommandInteraction,
-  ComponentType,
-  EmbedBuilder,
-  StringSelectMenuBuilder,
-} from "discord.js";
-import { ChatInputCommandData } from "../../type";
-import { CommandData as Command } from "../../function/handler";
-import config from "./../../config.js";
+    ActionRowBuilder,
+    ChatInputCommandInteraction,
+    ComponentType,
+    EmbedBuilder,
+    StringSelectMenuBuilder
+} from "discord.js"
+import { ChatInputCommandData } from "../../type"
+import { CommandData as Command } from "../../function"
+import config from "./../../config.js"
 
 export const data: ChatInputCommandData = {
-  name: "help",
-  description: "Replies with help stuff!",
-};
+    name: "help",
+    description: "Replies with help stuff!"
+}
 
 interface rawInfo {
-  folder: string;
-  cmd: string;
+    folder: string
+    cmd: string
 }
 
 export async function run(interaction: ChatInputCommandInteraction) {
-  const emojis: any = config.help_category_icon;
+    const emojis: any = config.help_category_icon
 
-  let rawInfo: Array<rawInfo> = [];
-  let commandInfo: any = [];
-  let selectInfo: any = [];
+    let rawInfo: Array<rawInfo> = []
+    let commandInfo: any = []
+    let selectInfo: any = []
 
-  Command().forEach((cmd) => {
-    rawInfo.push({
-      folder: cmd.path.category,
-      cmd: cmd.path.name,
-    });
-  });
+    Command().forEach((cmd) => {
+        rawInfo.push({
+            folder: cmd.path.category,
+            cmd: cmd.path.name
+        })
+    })
 
-  rawInfo.forEach((cmd) => {
-    const folder = cmd.folder.toLowerCase();
-    const existingEntry = commandInfo.find(
-      (entry: { value: string }) => entry.value === folder,
-    );
+    rawInfo.forEach((cmd) => {
+        const folder = cmd.folder.toLowerCase()
+        const existingEntry = commandInfo.find((entry: { value: string }) => entry.value === folder)
 
-    if (!existingEntry) {
-      commandInfo.push({
-        label: cmd.folder.charAt(0).toUpperCase() + cmd.folder.slice(1),
-        value: folder,
-        description: `Commands From ${cmd.folder} Category.`,
-        emoji: emojis[folder] ?? null,
-      });
-    }
-  });
+        if (!existingEntry) {
+            commandInfo.push({
+                label: cmd.folder.charAt(0).toUpperCase() + cmd.folder.slice(1),
+                value: folder,
+                description: `Commands From ${cmd.folder} Category.`,
+                emoji: emojis[folder] ?? null
+            })
+        }
+    })
 
-  const select = new StringSelectMenuBuilder()
-    .setCustomId("help")
-    .setPlaceholder("Choose Any Command Category!")
-    .addOptions(commandInfo);
+    const select = new StringSelectMenuBuilder()
+        .setCustomId("help")
+        .setPlaceholder("Choose Any Command Category!")
+        .addOptions(commandInfo)
 
-  const row = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
-    select,
-  );
+    const row = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(select)
 
-  const response = await interaction.reply({
-    content: "Which Category You Want Help In?",
-    components: [row],
-  });
+    const response = await interaction.reply({
+        content: "Which Category You Want Help In?",
+        components: [row]
+    })
 
-  const collector = response.createMessageComponentCollector({
-    componentType: ComponentType.StringSelect,
-    time: 300_000,
-  });
+    const collector = response.createMessageComponentCollector({
+        componentType: ComponentType.StringSelect,
+        time: 300_000
+    })
 
-  collector.on("collect", async (i) => {
-    const selection = i.values[0];
+    collector.on("collect", async (i) => {
+        const selection = i.values[0]
 
-    // Clear the selectInfo array
-    selectInfo = [];
+        // Clear the selectInfo array
+        selectInfo = []
 
-    // Filter rawInfo to get only the objects with matching 'folder' property
-    const matchingCommands = rawInfo.filter(
-      (cmd) => cmd.folder.toLowerCase() === selection,
-    );
+        // Filter rawInfo to get only the objects with matching 'folder' property
+        const matchingCommands = rawInfo.filter((cmd) => cmd.folder.toLowerCase() === selection)
 
-    // Push the matching objects into selectInfo
-    matchingCommands.forEach((cmd: any) => {
-      selectInfo.push({
-        name: cmd.cmd.data.name,
-        value: cmd.cmd.data.description,
-        folder: cmd.folder,
-      });
-    });
+        // Push the matching objects into selectInfo
+        matchingCommands.forEach((cmd: any) => {
+            selectInfo.push({
+                name: cmd.cmd.data.name,
+                value: cmd.cmd.data.description,
+                folder: cmd.folder
+            })
+        })
 
-    const embed = new EmbedBuilder()
-      .setAuthor({
-        name: interaction.client.user.username,
-        url: "https://discord.com/app",
-      })
-      .setTitle(`Category ${selection.toUpperCase()}`)
-      .addFields(selectInfo)
-      .setColor("Random")
-      .setFooter({
-        text: `Requested by ${i.user.username}`,
-      });
+        const embed = new EmbedBuilder()
+            .setAuthor({
+                name: interaction.client.user.username,
+                url: "https://discord.com/app"
+            })
+            .setTitle(`Category ${selection.toUpperCase()}`)
+            .addFields(selectInfo)
+            .setColor("Random")
+            .setFooter({
+                text: `Requested by ${i.user.username}`
+            })
 
-    await interaction.editReply({
-      components: [row],
-      embeds: [embed],
-    });
+        await interaction.editReply({
+            components: [row],
+            embeds: [embed]
+        })
 
-    i.deferUpdate();
-  });
+        i.deferUpdate()
+    })
 
-  collector.on("end", async () => {
-    interaction.editReply({
-      components: [],
-    });
-  });
+    collector.on("end", async () => {
+        interaction.editReply({
+            components: []
+        })
+    })
 }
