247ac9fa6f7de8c80b6bf8e0d37ab8f1fd57f367
Nandu <nanduwastaken@gmail.com>
2023-12-16 16:16:55 +0530

    Update commandRegister.js

--- handlers/commandRegister.js
+++ handlers/commandRegister.js
@@ -10,31 +10,51 @@ module.exports = {
       const Route = guildId
         ? Routes.applicationGuildCommands(client.user.id, guildId)
         : Routes.applicationCommands(client.user.id)
-      
+	    
         if (!guild) {
           console.error(
             `[❌] The bot has to join the guild with id "${guildId}" to register commands`,
           );
         }
+
+    const commands = [];
+    const foldersPath = path.join(__dirname, "../commands");
+    const commandFolders = fs.readdirSync(foldersPath);
+
+    for (const folder of commandFolders) {
+      const commandsPath = path.join(foldersPath, folder);
+      const commandFiles = fs
+        .readdirSync(commandsPath)
+        .filter((file) => file.endsWith(".js"));
+      for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const cmd = require(filePath);
+        if ("data" in cmd && "execute" in cmd) {
+          const duplicateCommand = commands.find(
+            (command) => command.name === cmd.data.name,
+          );
+          if (duplicateCommand) {
+            continue;
+          }
+          commands.push(cmd.data.toJSON());
+	}
       
 	const rest = new REST().setToken(client.token);
 
 	(async () => {
-		try {
-			console.log(`Started refreshing ${commands.length} application (/) commands.`);
-			const data = await rest.put(
-       	 Route,
-				{ body: commands },
-		);
-	console.log(
-        `[✅] Successfully registered ${localCommands.length} (/) commands ${mode}`,
-      );
+	  try {
+	     console.log(`Started refreshing ${commands.length} application (/) commands.`);
+	     const data = await rest.put(
+       	             Route,
+		     { body: commands },
+	     );
+	     console.log(
+      		  `[✅] Successfully registered ${localCommands.length} (/) commands ${mode}`,
+             );
 	} catch (error) {
-		console.error(`[❌] Error While Registering Commands: ${error}`);
+	     console.error(`[❌] Error While Registering Commands: ${error}`);
 	}
-})();
-
-      
-    });
-  },
+   })();
+  });
+ },
 };
